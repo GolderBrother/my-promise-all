30573631a60508fd5ab5bb74eb273113569868f0
GolderBrother <1204788939@qq.com>
2019-08-17 16:56:47 +0800

    Update 1.generator.js

--- 3.generator/1.generator.js
+++ 3.generator/1.generator.js
@@ -1,19 +1,20 @@
 // 生成器，用来生成迭代器的
 // 函数 里面的声明 *  yield 来实现
-// 会暂停
+// 只要遇到yield就会暂停，除非调用.next方法，再继续执行
 // function * read(){
 //     try{
-//         console.log(1);
-//         yield 1  // 产出
-//         console.log(2);
-//         yield 2
-//         console.log(3);
+//         console.log(1); // read() 1 {value: 1, done: false}
+//         let a = yield 1  // 产出
+//         console.log(2, a); //it.next() 2 undefined
+//         let b = yield 2
+//         console.log(3, b) // it.next('balabala') 3 "balabala" {value: undefined, done: true}
 //     }catch(err){
 //         console.log(err);
 //     }
 // }
 // let it = read(); // iterator
-// console.log(it.next());
+// 第一次next传入参数没有任何意义
+// console.log(it.next('first'));
 // it.throw('出错了'); // 抛出错误 让tryCatch 来捕获
 
 // function * buy(){
@@ -33,6 +34,8 @@
 // generator + promise 来使用
 let fs = require('fs').promises;
 
+// 生成器，用来生成迭代器的
+// const it = read(); * read() 返回值叫迭代器(it)：
 // promise 要通过then  generator 可以省略then方法
 function* read() {
     // try{
@@ -45,12 +48,16 @@ function* read() {
     // }
 }
 // co 库
+// let co = require('co');
+// 自己实现一个co库
 function co(it){
+    // 需要返回一个promise
     return new Promise((resolve,reject)=>{
         // 异步迭代 next
         function next(data){
            let {value,done} = it.next(data);
            if(!done){
+               // 包装成Promise
                Promise.resolve(value).then(data=>{
                   next(data)
                },reject);
@@ -58,6 +65,7 @@ function co(it){
                 resolve(value);
            }
         }
+        // 第一次next传入参数没有任何意义
         next();
     });
 }
@@ -85,7 +93,9 @@ co(read()).then(data=>{
 // async + await koa + async + await
 // react
 // 语法糖
+// fs的promise版本
 let fs = require('fs').promises
+// async 相当于 *,await 相当于 yield
 // async函数返回的是一个promise
 async function read() {
     try{
