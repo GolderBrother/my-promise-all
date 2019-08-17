5ab74f308480a76a9374007002592a4d39e43dda
GolderBrother <1204788939@qq.com>
2019-08-17 12:12:30 +0800

    Update 1.promise.js

--- 2.promise/history 2/1.promise.js
+++ 2.promise/history 2/1.promise.js
@@ -24,6 +24,27 @@ p.then((value)=>{ // fulfilled
 //     console.log('失败',reason);
 // });
 
+// 笔记
+// .then方法里面的逻辑就是收集函数,等待态的时候
+//每次调用resolve函数，就存储所有成功的回调
+//每次调用reject函数，就存储所有失败的回调
+// .then(res => {
+//     // 收集依赖
+// }, err => {
+//     // 收集依赖
+// })
+
+// 链式调用
+// promise.then方法的返回值
+// (1)普通值表示不是Promise，也不是错误
+// (2)如果返回promise，这个promise会执行，然后会采用他的状态，并返回给外层的then中
+// 成功(resolve态)就走then方法中的成功函数(resolve)
+// 失败(reject态 or throw new Error)就走then方法中的失败函数(reject)
+// (3).then方法返回的不是this，也就不是之前的promise，所以必须返回一个新的Promise
+
+// 让一个promise变为等待态(pending)
+// let p = new Promise(() => {});
+
 
 // then的链式调用 finnaly all race 测试咱们自己的promise
 // generator  co库
